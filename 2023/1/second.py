53a509ccfa19ec27ea67870870e49e6d35641283
Jozef Steinhübl (xHyroM) <xhyrom.hyro@gmail.com>
2023-12-02 09:05:23 +0100

    init

---
+++ 2023/1/second.py
@@ -0,0 +1,19 @@
+import re
+
+lines = []
+
+with open("./input.txt") as file:
+  for file_line in file:
+    lines.append(file_line.strip())
+
+count = 0
+
+for line in lines:
+  line = line.replace("one", "o1e").replace("two", "t2o").replace("three", "t3e").replace("four", "f4r").replace("five", "f5e").replace("six", "s6x").replace("seven", "s7n").replace("eight", "e8t").replace("nine", "n9e")
+  numbers = re.findall(rf'\d', line)
+  first_number = int(numbers[0])
+  last_number = int(numbers[-1])
+
+  count += first_number * 10 + last_number
+
+print(count)
