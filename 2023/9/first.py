b3a3a3b200c3cf7d0dd9dc694570d39b2f8d5a47
Jozef Steinhübl (xHyroM) <xhyrom.hyro@gmail.com>
2023-12-09 10:17:22 +0100

    day 9

---
+++ 2023/9/first.py
@@ -0,0 +1,48 @@
+from typing import List
+
+file = open("./input.txt").readlines()
+
+history: List[List[int]] = [
+    [int(item) for item in entry]
+    for entry in [x.split(" ") for x in (line.strip() for line in file)]
+]
+
+pyramids: List[List[int]] = []
+
+
+def get_sequence(entry: List[int]) -> List[int]:
+    sequence = []
+    for i, item in enumerate(entry):
+        previous = entry[i - 1]
+        sequence.append(item - previous)
+
+    sequence.pop(0)
+    return sequence
+
+
+for entry in history:
+    pyramids.append([])
+
+    pyramids[-1].append(entry)
+    sequence = get_sequence(entry)
+    pyramids[-1].append(sequence)
+    while not all(item == 0 for item in sequence):
+        sequence = get_sequence(sequence)
+        pyramids[-1].append(sequence)
+
+for pyramid in pyramids:
+    reversed_pyramid = list(reversed(pyramid))
+    for i, row in enumerate(reversed_pyramid):
+        if i == 0:
+            row.append(0)
+
+        next_row = reversed_pyramid[i + 1] if i + 1 < len(reversed_pyramid) else []
+        next_row.append(row[-1] + (next_row[-1] if len(next_row) > 0 else 0))
+
+
+count = 0
+
+for pyramid in pyramids:
+    count += pyramid[0][-1]
+
+print(count)
