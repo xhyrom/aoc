5cebad85dd3502fb58179c3fb71519ac1f5b0fed
Jozef Steinhübl (xHyroM) <xhyrom.hyro@gmail.com>
2023-12-17 10:35:51 +0100

    day 17

---
+++ 2023/17/second.py
@@ -0,0 +1,48 @@
+from heapq import heappush, heappop
+
+file = open("./input.txt").readlines()
+grid = [list(int(y) for y in list(x.strip())) for x in file]
+
+
+def add(queue, heat_loss: int, row: int, col: int, dr: int, dc: int, steps: int = 1):
+    new_row = row + dr
+    new_col = col + dc
+
+    if not (0 <= new_row < len(grid) and 0 <= new_col < len(grid[new_row])):
+        return
+
+    heappush(
+        queue,
+        (
+            heat_loss + grid[new_row][new_col],
+            new_row,
+            new_col,
+            dr,
+            dc,
+            steps,
+        ),
+    )
+
+
+visited = set()
+priority_queue = [(0, 0, 0, 0, 0, 0)]
+
+while priority_queue:
+    heat_loss, row, col, dr, dc, steps = heappop(priority_queue)
+
+    if steps >= 4 and row == len(grid) - 1 and col == len(grid[row]) - 1:
+        print(heat_loss)
+        break
+
+    if (row, col, dr, dc, steps) in visited:
+        continue
+
+    visited.add((row, col, dr, dc, steps))
+
+    if steps < 10 and (dr, dc) != (0, 0):
+        add(priority_queue, heat_loss, row, col, dr, dc, steps + 1)
+
+    if steps >= 4 or (dr, dc) == (0, 0):
+        for new_dr, new_dc in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+            if (new_dr, new_dc) != (dr, dc) and (new_dr, new_dc) != (-dr, -dc):
+                add(priority_queue, heat_loss, row, col, new_dr, new_dc)
