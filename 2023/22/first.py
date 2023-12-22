4d31e65abb44dc61a4dbc4e8100f526dda96966e
Jozef Steinhübl (xHyroM) <xhyrom.hyro@gmail.com>
2023-12-22 14:53:04 +0100

    day 22

---
+++ 2023/22/first.py
@@ -0,0 +1,53 @@
+from collections import defaultdict
+
+file = open("./input.txt").read().splitlines()
+
+bricks = []
+for line in file:
+    line = line.replace("~", ",").split(",")
+
+    bricks.append([int(item) for item in line])
+
+bricks.sort(key=lambda brick: brick[2])
+
+
+def intersects(a, b):
+    start_a, start_b = a[0], b[0]
+    end_a, end_b = a[3], b[3]
+
+    start_a_z, start_b_z = a[1], b[1]
+    end_a_z, end_b_z = a[4], b[4]
+
+    return max(start_a, start_b) <= min(end_a, end_b) and max(
+        start_a_z, start_b_z
+    ) <= min(end_a_z, end_b_z)
+
+
+for i, brick in enumerate(bricks):
+    max_z = 1
+    for previous_brick in bricks[:i]:
+        if intersects(brick, previous_brick):
+            max_z = max(max_z, previous_brick[5] + 1)
+
+    brick[5] -= brick[2] - max_z
+    brick[2] = max_z
+
+a_supports_b = defaultdict(set)
+b_supports_a = defaultdict(set)
+
+for upper_index, upper in enumerate(bricks):
+    for lower_index, lower in enumerate(bricks[:upper_index]):
+        if intersects(lower, upper) and upper[2] == lower[5] + 1:
+            a_supports_b[lower_index].add(upper_index)
+            b_supports_a[upper_index].add(lower_index)
+
+count = 0
+
+for i in range(len(bricks)):
+    for j in a_supports_b[i]:
+        if len(b_supports_a[j]) < 2:
+            break
+    else:
+        count += 1
+
+print(count)
