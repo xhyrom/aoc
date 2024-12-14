c692373af19d2eb0e5682d1797d1509d2d79d0da
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-12-14 08:19:04 +0100

    day 14 cleanup
    cleanup

--- 2024/14/solution.py
+++ 2024/14/solution.py
@@ -1,49 +1,21 @@
 from re import findall
-from typing import Optional
 
-Robot = tuple[list[int], list[int]]
+Robot = tuple[int, int, int, int]
 
 
 def get_robots() -> list[Robot]:
     return [
         (
-            [int(match[1]), int(match[0])],
-            [int(match[3]), int(match[2])],
-        )  # (row, col), (row_v, col_v)
+            int(match[1]),
+            int(match[0]),
+            int(match[3]),
+            int(match[2]),
+        )  # (row, col, row_v, col_v)
         for match in findall(
             r"p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)",
             open("input.txt").read(),
         )
     ]
-
-
-def is_valid(rows: int, cols: int, row: int, col: int):
-    return 0 <= row < rows and 0 <= col < cols
-
-
-def move_robots(
-    robots: list[Robot],
-    rows: int,
-    cols: int,
-    seen: Optional[set[tuple[int, int]]] = None,
-) -> Optional[set[tuple[int, int]]]:
-    for robot in robots:
-        new_row = robot[0][0] + robot[1][0]
-        new_col = robot[0][1] + robot[1][1]
-
-        if not (0 <= new_row < rows):
-            new_row = new_row % rows
-
-        if not (0 <= new_col < cols):
-            new_col = new_col % cols
-
-        robot[0][0] = new_row
-        robot[0][1] = new_col
-
-        if seen is not None:
-            seen.add((new_row, new_col))
-
-    return seen
 
 
 def quadrant_position(rows: int, cols: int, row: int, col: int) -> int:
@@ -71,13 +43,18 @@ def part_1() -> int:
     rows = 103
     cols = 101
 
-    for _ in range(100):
-        move_robots(robots, rows, cols)
+    for i, (row, col, row_v, col_v) in enumerate(robots):
+        robots[i] = (
+            (row + row_v * 100) % rows,
+            (col + col_v * 100) % cols,
+            row_v,
+            col_v,
+        )
 
     quadrants = {}
 
     for robot in robots:
-        pos = quadrant_position(rows, cols, *robot[0])
+        pos = quadrant_position(rows, cols, robot[0], robot[1])
         quadrants[pos] = quadrants.get(pos, 0) + 1
 
     result = 1
@@ -97,7 +74,22 @@ def part_2() -> int:
 
     i = 0
     while True:
-        seen = move_robots(robots, rows, cols, set())
+        seen = set()
+
+        for j, (row, col, row_v, col_v) in enumerate(robots):
+            new_row = row + row_v
+            new_col = col + col_v
+
+            if not (0 <= new_row < rows):
+                new_row = new_row % rows
+
+            if not (0 <= new_col < cols):
+                new_col = new_col % cols
+
+            robots[j] = (new_row, new_col, row_v, col_v)
+
+            seen.add((new_row, new_col))
+
         if seen is not None:
             if len(seen) == len(robots):
                 return i + 1  # to seconds
