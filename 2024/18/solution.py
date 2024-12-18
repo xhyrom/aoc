d5b1f052d92a3413490dad33ebe7ada31abc0f81
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-12-18 14:49:46 +0100

    day 18 optimize
    use normal bfs instead of A*, use set

--- 2024/18/solution.py
+++ 2024/18/solution.py
@@ -1,5 +1,5 @@
-from heapq import heappop, heappush
-from typing import Any, TypeAlias
+from collections import deque
+from typing import TypeAlias
 
 Position: TypeAlias = tuple[int, int]
 
@@ -11,38 +11,25 @@ def manhattan_distance(r1: int, c1: int, r2: int, c2: int) -> int:
 def solve(rows: int, cols: int, obstructions: list[Position]):
     end_row, end_col = rows - 1, cols - 1
 
-    start_h = manhattan_distance(0, 0, end_row, end_col)
-    pq = [(start_h, 0, 0, 0)]  # score + manhattan distance, score, row, col
+    queue = deque([(0, 0, 0)])  # row, col, distance
+    seen = {(0, 0)} | set(obstructions)
 
-    seen: dict[Position, float] = {}
-
-    while pq:
-        _, score, r, c = heappop(pq)
+    while queue:
+        r, c, dist = queue.popleft()
 
         if (r, c) == (end_row, end_col):
-            return score
-
-        if score >= seen.get((r, c), float("inf")):
-            continue
-
-        seen[(r, c)] = score
+            return dist
 
         for dr, dc in ((0, 1), (1, 0), (0, -1), (-1, 0)):
             new_r, new_c = r + dr, c + dc
-            if (
-                0 <= new_r < rows
-                and 0 <= new_c < cols
-                and (new_r, new_c) not in obstructions
-            ):
-                new_score = score + 1
-                new_h = manhattan_distance(new_r, new_c, end_row, end_col)
-
-                heappush(pq, (new_score + new_h, new_score, new_r, new_c))
+            if 0 <= new_r < rows and 0 <= new_c < cols and (new_r, new_c) not in seen:
+                seen.add((new_r, new_c))
+                queue.append((new_r, new_c, dist + 1))
 
     return -1
 
 
-def part_1() -> Any:
+def part_1() -> int:
     obstructions: list[Position] = [
         Position(map(int, x.split(",")[::-1]))  # row, col
         for x in open("input.txt").read().splitlines()
@@ -51,7 +38,7 @@ def part_1() -> Any:
     return solve(71, 71, obstructions[:1024])
 
 
-def part_2() -> Any:
+def part_2() -> str:
     obstructions: list[Position] = [
         Position(map(int, x.split(",")[::-1]))  # row, col
         for x in open("input.txt").read().splitlines()
