2174801237f3c24c286874afcddb788341d972b4
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-12-20 13:42:13 +0100

    day 07 optimize

--- 2024/07/solution.py
+++ 2024/07/solution.py
@@ -1,62 +1,73 @@
-from itertools import product
-
-
 def concatenate(a: int, b: int) -> int:
     digits = 0
     temp = b
     while temp > 0:
         temp //= 10
         digits += 1
-
     return a * (10**digits) + b
 
 
 def create_equation(line: str) -> tuple[list[int], int]:
     result, *numbers = line.split()
-    numbers = [int(x) for x in numbers]
-
-    return numbers, int(result[:-1])
+    return [int(x) for x in numbers], int(result[:-1])
 
 
-def evaluate(numbers: list[int], operators: tuple[str, ...], solution: int) -> int:
-    result = numbers[0]
+def solve_equation(
+    numbers: list[int],
+    target: int,
+    operators: list[str],
+    index: int = 0,
+    current_value: int = -1,
+):
+    if current_value == -1:
+        current_value = numbers[0]
+        index = 1
 
-    for i, operator in enumerate(operators):
+    if index == len(numbers):
+        if current_value == target:
+            yield []
+
+        return
+
+    if current_value > target:
+        return
+
+    for operator in operators:
         match operator:
             case "+":
-                result += numbers[i + 1]
+                new_value = current_value + numbers[index]
             case "*":
-                result *= numbers[i + 1]
+                new_value = current_value * numbers[index]
             case "||":
-                result = concatenate(result, numbers[i + 1])
+                new_value = concatenate(current_value, numbers[index])
+            case _:
+                raise ValueError(f"Invalid operator: {operator}")
 
-        if result > solution:
-            return -1
-
-    return result
+        for solution in solve_equation(
+            numbers, target, operators, index + 1, new_value
+        ):
+            yield [operator] + solution
 
 
 def part_1() -> int:
     equations = map(create_equation, open("input.txt").read().splitlines())
-
     total = 0
+
     for numbers, result in equations:
-        for operators in product(["+", "*"], repeat=len(numbers) - 1):
-            if evaluate(numbers, operators, result) == result:
-                total += result
-                break
+        for solution in solve_equation(numbers, result, ["+", "*"]):
+            total += result
+            break
 
     return total
 
 
 def part_2() -> int:
     equations = map(create_equation, open("input.txt").read().splitlines())
-
     total = 0
+
     for numbers, result in equations:
-        for operators in product(["+", "*", "||"], repeat=len(numbers) - 1):
-            if evaluate(numbers, operators, result) == result:
-                total += result
-                break
+        for solution in solve_equation(numbers, result, ["+", "*", "||"]):
+            total += result
+            break
 
     return total
