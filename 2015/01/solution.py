91846b7feb1c08e18dbef8a7694dc86d28692540
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-12-24 15:14:55 +0100

    y2015 day 01

--- 2015/01/solution.py
+++ 2015/01/solution.py
@@ -1,8 +1,32 @@
-from typing import Any
+from typing import Callable, TypeVar
+
+T = TypeVar("T")
 
 
-def part_1() -> Any:
-    pass
+def process(
+    instructions: str, early: Callable[[int, int], tuple[bool, T]] | None = None
+) -> T | int:
+    floor = 0
 
-def part_2() -> Any:
-    pass
+    for i, instruction in enumerate(instructions):
+        if instruction == "(":
+            floor += 1
+        elif instruction == ")":
+            floor -= 1
+
+        if early is not None:
+            met, value = early(floor, i)
+            if met:
+                return value
+
+    return floor
+
+
+def part_1() -> int:
+    instructions = open("input.txt").read()
+    return process(instructions)
+
+
+def part_2() -> int:
+    instructions = open("input.txt").read()
+    return process(instructions, lambda current_floor, i: (current_floor == -1, i + 1))
